424ebe329b0fb8c46dd49fe69ad7bdd4adc046cb
debasish saha <debasish1613@gmail.com>
2021-02-23 13:18:45 +0600

    Add task edit feature

--- js/app.js
+++ js/app.js
@@ -12,14 +12,16 @@ addButton.addEventListener('click', addNewTask);
 //functions
 function addNewTask()
 {
-    let toDoList;
-    if(localStorage.getItem(key) === null)
+    let toDoList = JSON.parse(localStorage.getItem(key)) || [];
+    if(toDoInput.value === '')
     {
-        toDoList = [];
+        alert("Please Input Some Text");
+        return;
     }
-    else
+    if(toDoList.includes(toDoInput.value))
     {
-        toDoList = JSON.parse(localStorage.getItem(key));
+        alert("This Task Already Exists");
+        return;
     }
     toDoList.push(toDoInput.value);
     localStorage.setItem(key, JSON.stringify(toDoList));
@@ -41,11 +43,19 @@ function createToDoItem(toDo)
     // create task item
     let taskItem = createElement('div', 'task-item');
     let taskText = createElement('li', 'task-text', toDo);
-    //append tasktext into task-item
+    //append taskText into task-item
     appendChild(taskItem, taskText);
+
+    //Edit feature
+    let editTask = createElement('button', 'edit-task', '<i class = "fas fa-edit"></i>');
+    editTask.setAttribute('title', 'Edit This Task');
+    editTask.addEventListener('click', editTaskIntoList);
+    appendChild(taskItem, editTask);
+
+    //delete feature.
     let deleteTask = createElement('button', 'delete-task', '<i class = "fas fa-trash"></i>');
     deleteTask.setAttribute('title', 'Delete This Task');
-    deleteTask.addEventListener('click', deleteTastFromList);
+    deleteTask.addEventListener('click', deleteTaskFromList);
     //append task delete button into task item.
     appendChild(taskItem, deleteTask);
 
@@ -67,11 +77,11 @@ function appendChild(parent, child)
     parent.appendChild(child);
 }
 
-function deleteTastFromList(e)
+function deleteTaskFromList(e)
 {
     let parent = this.parentElement;
-    let sibling = this.previousElementSibling;
-    let list = JSON.parse(localStorage.getItem(key));
+    let sibling = this.previousElementSibling.previousElementSibling; // this element refer to li element.
+    let list = JSON.parse(localStorage.getItem(key)); // todo list.
     let historyList = JSON.parse(localStorage.getItem('history')) || [];
     let index = list.indexOf(sibling.innerHTML);
     historyList.push(list[index]);
@@ -79,5 +89,30 @@ function deleteTastFromList(e)
     localStorage.setItem(key, JSON.stringify(list));
     localStorage.setItem('history', JSON.stringify(historyList));
     parent.remove();
+}
+
+function editTaskIntoList(e)
+{
+    let listItem = this.previousElementSibling; 
+    let listItemText = listItem.innerHTML;
+    listItem.innerHTML = '';
+    //Create input field for edit task.
+    let inputField = createElement('input', 'edit-input-field');
+    inputField.setAttribute('type', 'text');
+    inputField.value = listItemText;
+
+    inputField.addEventListener('keypress', addEditedTextIntoList);
+    function addEditedTextIntoList(e)
+    {
+        if(e.key === 'Enter')
+        {
+            let list = JSON.parse(localStorage.getItem(key));
+            let index = list.indexOf(listItemText);
+            list[index] = inputField.value;
+            localStorage.setItem(key, JSON.stringify(list));
+            listItem.innerHTML = inputField.value;
+        }
+    }
+    listItem.appendChild(inputField);
 }
 
